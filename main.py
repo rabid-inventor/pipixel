0a199e8855e130e0d4b83a8c97f62e6e13b4281e
Gee Bartlett <geeus81@gmail.com>
2014-06-09 17:12:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 
 import os, args
 from time import sleep as delay
+import spidev
 
 try:
 
   while 1:
+    spi = spidev.SpiDev()
+    spi.open(0,0)
+    spi.max_speed_hz=(16000000)
     delay(10)
+    print(spi.SpiDev_get_mode())
+    
+    
   
   
